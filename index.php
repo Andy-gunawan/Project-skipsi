5d2f0210287cffa996b2500f6653f6210e4490f5
Andy-gunawan <s4ndys4putr403@gmail.com>
2022-12-13 17:50:25 +0700

    upload project

---
+++ index.php
@@ -0,0 +1,20 @@
+<html>
+<head>
+<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+    <body>
+    <img src="pexels.jpg">
+    <div class="container">
+        <div class="row">
+        <h1> FIND AND RENTAL CAR IN EASY STEP</h1>
+        
+        </div>  
+    </div>
+    </body>
+</head>
+</html>
+
+
